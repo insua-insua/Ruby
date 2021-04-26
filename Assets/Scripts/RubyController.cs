caeaf5ca56bb4a6c1c1b7df9c5f7ffdc4a4e3a3d
IN SUA <thkswain@gmail.com>
2021-04-26 19:45:16 +0900

    키보드 입력과 속도

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,21 +4,19 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start는 처음 시작되는 부분 - Git테스트중
-    // 가사가 다시 수정
+    // Start is called before the first frame update
     void Start()
     {
-        
     }
-
-    // 화면 갱신될때마다 호출됨 - git테스트중
+    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal + "그리고 다른 부분을 추가 수정했다 - git test중");
-
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
+
