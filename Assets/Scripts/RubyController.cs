84fcedf7b326014a038fbac9bfe3b789f38aea9b
IN SUA <thkswain@gmail.com>
2021-04-29 16:33:30 +0900

    1교시 수업

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -16,10 +16,14 @@ public class RubyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
     }
@@ -30,9 +34,22 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+
         Vector2 position = rigidbody2d.position;
-        position.x += speed * horizontal * Time.deltaTime;
-        position.y += speed * vertical * Time.deltaTime;
+       
+        position += move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
